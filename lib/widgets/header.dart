a46ba04890848895f020195c25a5055fef660d3d
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2020-12-19 15:30:34 -0500

    add reusable widget

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
-header() {
-  return Text("header");
+AppBar header(BuildContext context,
+    {bool isAppTitle = false, String titleText}) {
+  return AppBar(
+    title: Text(
+      isAppTitle ? "Fluttershare" : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? "Signatra" : "",
+        fontSize: isAppTitle ? 50.0 : 25.0,
+      ),
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
 }
