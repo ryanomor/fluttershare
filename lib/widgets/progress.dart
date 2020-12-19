1891057f86c9ef668b7896da026ff438b476cff1
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2020-12-19 16:19:01 -0500

    add reusable progress widget

--- lib/widgets/progress.dart
+++ lib/widgets/progress.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
-circularProgress() {
-  return Text("circular progress");
+Container circularProgress(BuildContext context) {
+  return Container(
+    alignment: Alignment.center,
+    // padding: EdgeInsets.only(top: 10.0),
+    child: CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Theme.of(context).primaryColor),
+    ),
+  );
 }
 
-linearProgress() {
-  return Text("linear progress");
+Container linearProgress(BuildContext context) {
+  return Container(
+    // alignment: ,
+    child: LinearProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(Theme.of(context).primaryColor),
+    ),
+  );
 }
