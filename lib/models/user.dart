b2dd88461d3e56161d0ce5ae6642afdb73c05e3a
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2020-12-21 18:19:36 -0500

    add user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,1 +1,29 @@
-class User {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  final String id;
+  final String username;
+  final String email;
+  final String photoUrl;
+  final String displayName;
+  final String bio;
+
+  User(
+      {this.id,
+      this.username,
+      this.email,
+      this.bio,
+      this.displayName,
+      this.photoUrl});
+
+  factory User.fromDocument(DocumentSnapshot doc) {
+    return User(
+      id: doc["id"],
+      username: doc["username"],
+      email: doc["email"],
+      bio: doc["bio"],
+      displayName: doc["displayName"],
+      photoUrl: doc["photoUrl"],
+    );
+  }
+}
