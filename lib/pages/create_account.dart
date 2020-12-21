aaa659b2ffe476874568275d26bda4e720a00074
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2020-12-21 18:22:08 -0500

    add initial create username page

--- lib/pages/create_account.dart
+++ lib/pages/create_account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttershare/widgets/header.dart';
 
 class CreateAccount extends StatefulWidget {
   @override
@@ -6,8 +7,72 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  final _formKey = GlobalKey<FormState>();
+  String username;
+
+  submit() {
+    _formKey.currentState.save();
+    Navigator.pop(context, username);
+  }
+
   @override
-  Widget build(BuildContext parentContext) {
-    return Text("Create Account");
+  Scaffold build(BuildContext parentContext) {
+    return Scaffold(
+      appBar: header(context, titleText: "Create Account"),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 25.0),
+                  child: Text(
+                    "Username",
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Container(
+                    child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        onSaved: (newValue) => username = newValue,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Username",
+                            labelStyle: TextStyle(fontSize: 15.0),
+                            hintText: "Must be at least 3 characters"),
+                      ),
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: submit,
+                  child: Container(
+                    height: 50.0,
+                    width: 350.0,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        borderRadius:
+                            BorderRadius.circular(7.0) // round out box corners
+                        ),
+                    child: Center(
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
