d085135eebb70cbe2e2fd25cd1fcbc9679ed3ef0
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2020-12-31 14:48:47 -0500

    add upload functionality

--- lib/pages/upload.dart
+++ lib/pages/upload.dart
@@ -1,13 +1,288 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:fluttershare/models/user.dart';
+import 'package:fluttershare/pages/home.dart';
+import 'package:fluttershare/widgets/progress.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:image/image.dart' as Im;
+import 'package:uuid/uuid.dart';
 
 class Upload extends StatefulWidget {
+  final User currentUser;
+
+  Upload({this.currentUser});
+
   @override
   _UploadState createState() => _UploadState();
 }
 
 class _UploadState extends State<Upload> {
+  TextEditingController captionController = TextEditingController();
+  TextEditingController locationController = TextEditingController();
+  File file;
+  bool isUploading = false;
+  String postId = Uuid().v4();
+
+  handleTakePhoto() async {
+    Navigator.pop(context); // this will take the user back one screen
+
+    File file = await ImagePicker.pickImage(
+        source: ImageSource.camera, maxHeight: 675, maxWidth: 960);
+
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  handleFromGallery() async {
+    Navigator.pop(context);
+
+    File file = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      this.file = file;
+    });
+  }
+
+  selectImage(BuildContext parentContext) {
+    return showDialog(
+        context: parentContext,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text("Create Post"),
+            children: [
+              SimpleDialogOption(
+                child: Text("Take with Camera"),
+                onPressed: handleTakePhoto,
+              ),
+              SimpleDialogOption(
+                child: Text("Choose from Gallery"),
+                onPressed: handleFromGallery,
+              ),
+              SimpleDialogOption(
+                child: Text("Cancel"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        });
+  }
+
+  Container buildSplashScreen() {
+    return Container(
+      color: Theme.of(context).primaryColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(
+            "assets/images/upload.svg",
+            height: 260.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Text(
+                "Upload Image",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                ),
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () => selectImage(context),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  clearImage() {
+    setState(() {
+      this.file = null;
+    });
+  }
+
+  compressImage() async {
+    final tempDir = await getTemporaryDirectory();
+    final path = tempDir.path;
+
+    // Im.Image imageFile = Im.decodeImage(file.readAsBytesSync());
+    final File compressedImgFile =
+        await FlutterImageCompress.compressAndGetFile(
+            file.absolute.path, "$path/img_$postId.jpg",
+            quality: 85);
+    // File('$path/img_$postId.jpg')
+    // ..writeAsBytesSync(Im.encodeJpg(imageFile, quality: 85));
+
+    setState(() {
+      file = compressedImgFile;
+    });
+  }
+
+  sendPostToFirestore({String mediaUrl, String location, String description}) {
+    postsRef
+        .document(widget.currentUser.id)
+        .collection("usersPosts")
+        .document(postId)
+        .setData({
+      "postId": postId,
+      "ownerId": widget.currentUser.id,
+      "username": widget.currentUser.username,
+      "mediaUrl": mediaUrl,
+      "description": description,
+      "location": location,
+      "timestamp": DateTime.now(),
+      "likes": {}
+    });
+  }
+
+  Future<String> uploadImage(File imgFile) async {
+    StorageUploadTask uploadTask =
+        storageRef.child("post_$postId.jpg").putFile(imgFile);
+    StorageTaskSnapshot storageSnap = await uploadTask.onComplete;
+    String downloadUrl = await storageSnap.ref.getDownloadURL();
+
+    return downloadUrl;
+  }
+
+  handleSubmit() async {
+    await compressImage();
+
+    setState(() {
+      isUploading = true;
+    });
+
+    String mediaUrl = await uploadImage(file);
+    sendPostToFirestore(
+        mediaUrl: mediaUrl,
+        location: locationController.text,
+        description: captionController.text);
+
+    // reset state
+    captionController.clear();
+    locationController.clear();
+    setState(() {
+      file = null;
+      isUploading = false;
+      postId = Uuid().v4();
+    });
+  }
+
+  Scaffold buildUploadForm() {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white70,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: clearImage,
+        ),
+        title: Text(
+          "Caption Post",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          FlatButton(
+            onPressed: isUploading ? null : () => handleSubmit(),
+            child: Text(
+              "Post",
+              style: TextStyle(
+                  color: Theme.of(context).accentColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          isUploading ? linearProgress(context) : Text(""),
+          Container(
+            height: 220.0,
+            width: MediaQuery.of(context).size.width *
+                0.8, // to take up 80% of the current width of screen
+            child: Center(
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: FileImage(file),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(top: 10.0)),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage:
+                  CachedNetworkImageProvider(widget.currentUser.photoUrl),
+            ),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                controller: captionController,
+                decoration: InputDecoration(
+                  hintText: "Add a caption",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(
+              Icons.pin_drop,
+              color: Theme.of(context).primaryColor,
+              size: 24.0,
+            ),
+            title: Container(
+              width: 250.0,
+              child: TextField(
+                controller: locationController,
+                decoration: InputDecoration(
+                  hintText: "Photo Location",
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 100.0,
+            width: 200.0,
+            alignment: Alignment.center,
+            child: RaisedButton.icon(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0)),
+              onPressed: () => print("Get user location"),
+              icon: Icon(
+                Icons.my_location,
+                color: Colors.redAccent,
+              ),
+              label: Text("Use current location",
+                  style: TextStyle(color: Colors.white)),
+              color: Colors.blueAccent,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Upload");
+    return file == null ? buildSplashScreen() : buildUploadForm();
   }
 }
