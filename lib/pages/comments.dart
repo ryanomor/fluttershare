5f1bd608cfcc721b804f66da1519ef700b85b820
Ryan Omoruyi <omoruyi.ryan@gmail.com>
2021-01-10 18:23:52 -0500

    add display post's comments

--- lib/pages/comments.dart
+++ lib/pages/comments.dart
@@ -1,6 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/pages/home.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class Comments extends StatefulWidget {
   final String postId;
@@ -34,7 +38,27 @@ class CommentsState extends State<Comments> {
   });
 
   displayPostsComments() {
-    return Text("Post's Comments");
+    return StreamBuilder(
+      stream: commentsRef
+          .document(postId)
+          .collection("comments")
+          .orderBy("timestamp", descending: true)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress(context);
+        }
+
+        List<Comment> comments = [];
+        snapshot.data.documents.forEach((doc) {
+          comments.add(Comment.fromDocument(doc));
+        });
+
+        return ListView(
+          children: comments,
+        );
+      },
+    );
   }
 
   addComment() {
@@ -77,8 +101,43 @@ class CommentsState extends State<Comments> {
 }
 
 class Comment extends StatelessWidget {
+  final String username;
+  final String userId;
+  final String avatarUrl;
+  final String comment;
+  final Timestamp timestamp;
+
+  Comment({
+    this.username,
+    this.userId,
+    this.avatarUrl,
+    this.comment,
+    this.timestamp,
+  });
+
+  factory Comment.fromDocument(DocumentSnapshot doc) {
+    return Comment(
+      username: doc['username'],
+      userId: doc['userId'],
+      avatarUrl: doc['avatarUrl'],
+      comment: doc['comment'],
+      timestamp: doc['timestamp'],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Comment');
+    return Column(
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            backgroundImage: CachedNetworkImageProvider(avatarUrl),
+          ),
+          title: Text(comment),
+          subtitle: Text(timeago.format(timestamp.toDate())),
+        ),
+        Divider()
+      ],
+    );
   }
 }
